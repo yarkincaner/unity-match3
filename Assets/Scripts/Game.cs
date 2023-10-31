bc8d6607ae10a97a43b0cd1ac7d2f9ce70490d6f
yarkincaner <yarkincaner@gmail.com>
2023-10-31 22:32:18 +0300

    finished with coroutine

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,40 +7,53 @@ public class Game : MonoBehaviour
 {
     public Material red;
     public Material blue;
-    public float delay = 3;
+    public float delay = 3.0f; // Delay between cube creation
     public int maxSize = 8;
 
     private List<GameObject> gameObjects;
     private int pointer;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObjects = new List<GameObject>();
         pointer = -1;
-
+        //createCube();
         StartCoroutine(spawnObject());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pointer == maxSize - 1) {
-            destroyObjects(2);
-        }
+        
     }
 
-    void destroyObjects(int index)
-    {
-        for (int i = index; i > index - 3; i--)
-        {
-            Destroy(gameObjects[i]);
-            gameObjects.RemoveAt(i);
-            pointer--;
-        }
-    }
+    // Tried to create new cube when collision happens but it did not work :(
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (pointer == maxSize - 1)
+    //    {
+    //        destroyObjects(2);
+    //    }
+
+    //    if (pointer >= 2)
+    //    {
+    //        if (checkMatch())
+    //        {
+    //            destroyObjects(pointer);
+    //        }
+    //    }
+
+    //    createCube();
+    //}
 
     // Coroutine that spawns a cube every second
-    IEnumerator spawnObject() {
+    IEnumerator spawnObject()
+    {
+        if (pointer == maxSize - 1)
+        {
+            destroyObjects(2);
+        }
         createCube();
         yield return new WaitForSeconds(delay);
         if (pointer >= 2)
@@ -54,7 +67,7 @@ public class Game : MonoBehaviour
         StartCoroutine(spawnObject());
     }
 
-    Boolean checkMatch()
+    public Boolean checkMatch()
     {
         if (gameObjects[pointer].tag == gameObjects[pointer-1].tag && gameObjects[pointer].tag == gameObjects[pointer - 2].tag)
         {
@@ -62,15 +75,27 @@ public class Game : MonoBehaviour
         }
         return false;
     }
+    public void destroyObjects(int index)
+    {
+        for (int i = index; i > index - 3; i--)
+        {
+            Destroy(gameObjects[i]);
+            gameObjects.RemoveAt(i);
+            pointer--;
+        }
+    }
 
-    void createCube()
+    public void createCube()
     {
         int randomNum = UnityEngine.Random.Range(0, 2);
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = new Vector3(0, 10, 0);
+        cube.transform.position = new Vector3(0, 8, 0);
         cube.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
         Rigidbody currentRb = cube.AddComponent<Rigidbody>();
         currentRb.detectCollisions = true;
+        Collider collider = currentRb.GetComponent<Collider>();
+        collider.material.bounciness = 0;
+        cube.transform.parent = this.transform;
 
         switch (randomNum)
         {
---
+++ README.md
@@ -0,0 +1,7 @@
+# Match-3 Game Created With Unity
+
+A one row Match-3 game.
+
+In initial state, game randomly generates a Cube with one of two colors (red or blue). After each second, a new cube will be generated. If there are more than 2 cubes in the scene, possible match is checked; if there are three consqutive matches these three objects are being destroyed, if not, it contious to generate cubes and check if there are consequtive matches each time. If number of cubes reached max size (in this case it is 8), the three cubes at the bottom are destroyed and the game continues.
+
+## Example Video
